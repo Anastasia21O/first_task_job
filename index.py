14253635747f99ef9f5da7141b15e7fa6eddbd06
Anastasia21O <virstanastya21@gmail.com>
2022-07-03 19:40:26 +0300

    (add) finish main work

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
-from scripts.main import mass_query_first, mass_query_sod
+from scripts.main import mass_query_first, mass_query_sod, mass_query_all
 
 app = Flask(__name__)
 app.run(debug=True)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', mass=mass_query_all)
 
 @app.route('/task/<int:number_task>')
 def show_table(number_task):
